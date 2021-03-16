92dd2c46b5de7caa7b03418d358a925facc7e3ba
younggg96 <yangguannggeng960123@gmail.com>
2021-03-16 17:51:57 -0400

    new update

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,20 +1,23 @@
-import React from "react";
-import { Box, Button, Grid, makeStyles, Typography } from "@material-ui/core";
+import React, { useState } from "react";
+import { Box, Button, Divider, Grid, makeStyles, Slider, Typography } from "@material-ui/core";
 import ChooseOptions from "./chooseOptions";
+// icon
+import DoneIcon from '@material-ui/icons/Done';
+// img
 import prod from "../../assets/imgs/product.png";
+//scss
 import "../../scss/content.scss";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: "#fff",
     borderRadius: theme.shape.borderRadius,
+    backgroundColor: '#fff',
     marginTop: theme.spacing(4),
     width: "100%",
   },
   choose: {
-    borderRadius: '16px 0 0 16px',
-    backgroundColor: theme.palette.primary.main,
-    padding: theme.spacing(4),
+    borderRadius: '16px 0px 0px 16px',
+    padding: theme.spacing(3, 1),
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
@@ -22,13 +25,14 @@ const useStyles = makeStyles((theme) => ({
   },
   order: {
     backgroundColor: theme.palette.primary.main,
-    padding: theme.spacing(6),
+    padding: theme.spacing(4, 6),
   },
   orderTitle: {
     color: theme.palette.secondary.light,
+    lineHeight: 1,
   },
   orderButton: {
-    padding: "16px 72px",
+    padding: "12px 64px",
     borderRadius: 32,
     color: theme.palette.secondary.light,
     borderColor: theme.palette.secondary.light,
@@ -37,23 +41,101 @@ const useStyles = makeStyles((theme) => ({
   content: {
     color: theme.palette.secondary.light,
     padding: theme.spacing(2, 6),
-    width: 360,
+    width: 400,
   },
+  doneIcon: {
+    margin: theme.spacing(1, 1.5),
+    backgroundColor: theme.palette.primary.dark,
+    borderRadius: "50%",
+    padding: theme.spacing(1),
+  },
+  steps: {
+    padding: theme.spacing(4, 2),
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  title: {
+    lineHeight: 1,
+    fontWeight: "bolder"
+  },
+  slider: {
+    marginTop: theme.spacing(2)
+  },
+  chooseTab: {
+    marginTop: theme.spacing(4)
+  }
 }));
+
+const SliderBar = () => {
+  const classes = useStyles();
+  const [currentDistance, setCurrentDistance] = useState(22);
+  const handleSliderChange = (event, newValue) => {
+    console.log(event.target.value)
+    setCurrentDistance(newValue);
+  };
+
+  return (
+    <React.Fragment>
+      <Grid container className={classes.slider} direction="column" style={{ paddingRight: 40 }}>
+        <Grid item container justify="space-between" alignItems="center">
+          <Grid item xs>
+            <Typography variant="h6" component="span">CHOOSE <Box component="span" color="primary.main">DISTANCE</Box></Typography>
+          </Grid>
+          <Grid item container xs={4} justify="flex-end" alignItems="center">
+            <Grid item xs style={{ textAlign: "end" }}>
+              <Typography style={{lineHeight: 1}}>CURRENT <br />DISTANCE </Typography>
+            </Grid>
+            <Grid item xs style={{ textAlign: "end" }}>
+              <Typography variant="h3" component="span" style={{ fontWeight: "bolder"}}>{currentDistance}</Typography>
+              <Box color="primary.main" component="span" style={{ fontWeight: "500", fontSize: "28px"}}>m</Box>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Slider
+            value={currentDistance}
+            onChange={handleSliderChange}
+          />
+        </Grid>
+        <Grid item container justify="space-between" alignItems="center">
+          <Typography variant="body1"><Box color="primary.dark" fontWeight="600">0m</Box></Typography>
+          <Typography variant="body1"><Box color="primary.dark" fontWeight="600">50m</Box></Typography>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  )
+}
+
+const TabItem = () => {
+  const classes = useStyles();
+  return (
+    <div></div>
+  )
+}
+
+const ChooseTab = () => {
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Grid></Grid>
+    </React.Fragment>
+  )
+}
 
 const Content = () => {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <div className={`${classes.root} bg-whole`}>
-        <Grid container>
-          <Grid item xs={1} className={classes.choose}>
+      <Grid container className={classes.root}>
+        <Grid container item xs={6} className="bg-clip">
+          <Grid item xs={2} className={classes.choose}>
             <ChooseOptions />
           </Grid>
-          <Grid item container xs={4} direction="column" className="bg-clip">
+          <Grid item container xs direction="column" style={{ position: "relative" }}>
             <Grid item container className={classes.order}>
               <Grid item xs="5">
-                <Typography variant="h5" className={classes.orderTitle}>
+                <Typography variant="h6" className={classes.orderTitle}>
                   <Box fontWeight="bolder">
                     WHITE LABEL
                     <br /> W-394900 XP
@@ -68,7 +150,7 @@ const Content = () => {
             </Grid>
             <Grid item className={classes.content}>
               <Typography
-                variant="body2"
+                variant="body1"
                 component="p"
                 style={{ marginBottom: 16 }}
               >
@@ -90,17 +172,45 @@ const Content = () => {
               </Typography>
             </Grid>
             <Grid item className="live-view">
-              <Typography variant="h4" component="div">
+              <Typography variant="h5" component="div">
                 LIVE<br /> VIEW
               </Typography>
             </Grid>
           </Grid>
-          <Grid item xs>
-            hhahahahh
-            <img className="prod" src={prod} alt="prod" width="450px"/>
-          </Grid>
         </Grid>
-      </div>
+        <Grid item container xs={6} direction="column" style={{ position: "relative" }}>
+          <Grid item container direction="column" className={classes.steps}>
+            <Grid item direction="row" style={{ display: "flex" }}>
+              {[0, 1, 2, 3].map(ele => {
+                if (ele === 2) return (<div key={ele} className={`${classes.doneIcon} now-step`}>
+                  <DoneIcon className="done-svg now-step" />
+                </div>)
+                return (<div key={ele} className={classes.doneIcon}>
+                  <DoneIcon className="done-svg" />
+                </div>)
+              })}
+            </Grid>
+            <Grid item>
+              <Typography variant="subtitle1" style={{ padding: "8px 16px", fontWeight: "bolder", color: "#00000080" }}>Complete 3 steps of 4</Typography>
+            </Grid>
+          </Grid>
+          <Grid item container direction="column">
+            <Typography variant="h4" className={classes.title}>
+              TRANSMISSION
+               <Box color="primary.main">DISTANCE</Box>
+            </Typography>
+          </Grid>
+          <Grid item className={classes.slider}><SliderBar /></Grid>
+          <Divider />
+          <Grid item className={classes.chooseTab}>
+            <Grid item xs>
+              <Typography variant="h6" component="span">CHOOSE <Box component="span" color="primary.main">TYPE OF CONNECTION</Box></Typography>
+              <ChooseTab />
+            </Grid>
+          </Grid>
+          <img className="prod" src={prod} alt="prod" />
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 };
